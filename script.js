b079e38989c2f3f9851c34a5d16ab6778a5cb297
Isaac Bernat <isaac.bernat@gmail.com>
2025-06-15 11:28:20 +0200

    feat(v01): retina and non-retina versions for landing image

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 document.addEventListener("DOMContentLoaded", function() {
     const lazyImage = document.querySelector('.main-image.lazy-load');
+    if (!lazyImage || !lazyImage.dataset.srcset) {
+        return;
+    }
 
-    if (lazyImage) {
-        const highResImage = new Image();  // Create a new image object in memory
-        highResImage.src = lazyImage.dataset.src; // Start loading the high-res image
+    const sourceElement = lazyImage.previousElementSibling;  // Find the <source> element that is the sibling of the <img>
+    const srcsetValue = lazyImage.dataset.srcset;
 
-        highResImage.onload = function() {  // Once the high-res image is fully loaded, replace the low-res one
-            lazyImage.src = highResImage.src;  // Swap the src to the high-res version
-            lazyImage.classList.remove('lazy-load');  // Remove the 'lazy-load' class to remove the blur
-        };
+    const onImageLoad = () => {
+        lazyImage.classList.remove('lazy-load');  // Remove the blur effect
+        lazyImage.removeEventListener('load', onImageLoad);  // Clean up the event listener so it doesn't fire again
+    };
+    lazyImage.addEventListener('load', onImageLoad);  // Fire AFTER the browser has chosen the correct image from the srcset, downloaded it, and is ready to display it.
+    if (sourceElement) {  // let browser CHOOSE the most appropriate image to download according to screen's pixel density
+        sourceElement.srcset = srcsetValue;
+    }
+    const fallbackSrc = srcsetValue.split(',')[0].split(' ')[0];
+    lazyImage.src = fallbackSrc;  // 1x image for `src` for older browsers that don't understand <picture> or for other edge cases.
+
+    if (lazyImage.complete) {  // Handle cached images: fire the 'load' event
+        onImageLoad();
     }
 });
